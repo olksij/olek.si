21568e4235b2f3d9b673aaa7cdfa869751365c15
Alex Besida <pixel.coder7@gmail.com>
2019-08-06 13:54:55 +0300

    Testing portfolio..

--- About/Notes/main.js
+++ About/Notes/main.js
@@ -1,23 +1,19 @@
 var SSIDs1 = new Array('PSProjectsBar','PSPBScrollRight','PSPBScrollLeft');
-var SSIDs2 = new Array('DSSocialBar','DSScrollRight','DSScrollLeft');
-var SSIDs3 = new Array('TGTSThemesBar','TGTSScrollRight','TGTSScrollLeft');
-var SSIDs4 = new Array('GVTSThemesBar','GVTSScrollRight','GVTSScrollLeft');
 
 function Scroll(a) { location.hash="#TopSlide"; location.hash=a; }
 
 function Start(){ 
-    CSB('!',SSIDs1); CSB('!',SSIDs2); CSB('!',SSIDs3); CSB('!',SSIDs4);
+    CSB('!',SSIDs1);
     Resize(); 
 }
 
 function CSB(a,SSID) {
-    if ( a == '!' ){ CSBA(SSID[0],SSID[1],SSID[2],document.getElementById(SSID[0]).scrollLeft); }
+    if ( a == '!' ){ /*CSBA(SSID[0],SSID[1],SSID[2],document.getElementById(SSID[0]).scrollLeft);*/ }
     else {
         var SSFV = document.getElementById(SSID[0]).scrollLeft;
         if (a=='R') { SSFV += document.getElementById('body').offsetWidth/1.5; } else { SSFV -= document.getElementById('body').offsetWidth/1.5; }  
         if (SSFV < 0) { SSFV = 0 } else if ( SSFV > (document.getElementById(SSID[0]).scrollWidth - document.getElementById(SSID[0]).clientWidth)) { SSFV = document.getElementById(SSID[0]).scrollWidth - document.getElementById(SSID[0]).clientWidth }
-        CSBA(SSID[0],SSID[1],SSID[2],SSFV);
-        document.getElementById(SSID[0]).scrollLeft = SSFV;
+        CSBA(SSID[0],SSID[1],SSID[2],SSFV); document.getElementById(SSID[0]).scrollLeft = SSFV;
     }
 }
 
@@ -47,4 +43,15 @@ function CSBA(BarID,SRightID,SLeftID,FTSLP) {
     }
 }
 
-function Resize() { CSB('!',SSIDs1); CSB('!',SSIDs2); CSB('!',SSIDs3); CSB('!',SSIDs4); }
+function Resize() { /*CSB('!',SSIDs1);*/ 
+    if (document.getElementById('body').offsetWidth > document.getElementById('body').offsetHeight/95*100) {
+        document.getElementById("TSBGI").style.left = (0 - document.getElementById('body').offsetWidth/4)+'px';
+        document.getElementById("TSBGI").style.width = document.getElementById('body').offsetWidth+'px';
+        document.getElementById("TSBGI").style.height = (document.getElementById('body').offsetWidth/100*95)+'px';
+    } else {
+        document.getElementById("TSBGI").style.width = (document.getElementById('body').offsetHeight/95*100)+'px';
+        document.getElementById("TSBGI").style.height = document.getElementById('body').offsetHeight+'px';
+        document.getElementById("TSBGI").style.left = ((document.getElementById('body').offsetWidth-document.getElementById("TSBGI").offsetWidth)/2)+'px';
+    }
+    document.getElementById("TSBGI").style.top = ((document.getElementById('body').offsetHeight-document.getElementById("TSBGI").style.height.slice(0,document.getElementById("TSBGI").style.height.length-2))/2);
+}

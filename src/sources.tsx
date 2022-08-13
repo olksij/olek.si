afbc98b52bf3271bfe5bcc336e9fd53371abb119
Oleksii Besida <pixel.coder7@gmail.com>
2022-08-13 09:58:10 +0200

    render improvements

--- src/sources.tsx
+++ src/sources.tsx
@@ -1,8 +1,9 @@
 import { createElement, createFragment } from "./jsx";
 
 // dates with custom description
-export const dates = {
-  "5-12": ["🎂 It's my birthday today!", "June 12"],
+export const dates: Record<string, Array<string>> = {
+  "6-12": ["🎂 It's my birthday today!", "June 12"],
+  "8-24": ["Restoration of independence of Ukraine", "🇺🇦 August 24"],
   // more dates to come such as celebrations and holidays
 };
 
@@ -13,23 +14,22 @@ export const head: Node[] = [
 ];
 
 // font links to load
-export const fonts = {
+export const fonts: Record<string, URL> = {
   'Google Sans Display': new URL('/assets/fonts/GoogleSansDisplay-Bold.woff2', import.meta.url),
   'Google Sans Text': new URL('/assets/fonts/GoogleSansText-Medium.ttf', import.meta.url),
 };
 
 // image links to load
-export const images = {
-  "profilePicture": new URL("/assets/profilePicture.jpg?as=webp&width=512", import.meta.url),
+export const images: Record<string, URL> = {
+  "pf": new URL("/assets/profilePicture.jpg?as=webp&width=512", import.meta.url),
   "telegram": new URL("/assets/telegram.svg", import.meta.url),
   "instagram": new URL("/assets/instagram.svg", import.meta.url),
   "github": new URL("/assets/github.svg", import.meta.url),
   "twitter": new URL("/assets/twitter.svg", import.meta.url),
   "email": new URL("/assets/email.svg", import.meta.url),
-  "copyright": new URL("/assets/copyright.svg", import.meta.url),
-  "123": new URL('/assets/oleksii.svg', import.meta.url),
+  "cr": new URL("/assets/copyright.svg", import.meta.url),
 };
 
-export const stylesheets = [
+export const stylesheets: URL[] = [
   new URL("./index.css", import.meta.url)
 ];

1e3240a2f5827074e5a75c2779c46a0990a91649
Oleksii Besida <pixel.coder7@gmail.com>
2022-09-18 11:07:37 +0200

    improved menu appearance

--- src/modules/render.tsx
+++ src/modules/render.tsx
@@ -164,9 +164,38 @@ computeWorker.onmessage = (message) => {
   if (message.data.deliver == 'texts') resolveMorph(message.data.data as TextsRecord);
 }
 
+let menuOpenBgKeyframes: Keyframe[] = [
+  {
+    backgroundPosition: '0px 0px',
+    boxShadow: 'inset 0px 0px 0px 50vh #FFF0',
+  },
+  {
+    backgroundPosition: '-32px 0px',
+    boxShadow: 'inset 0px 0px 0px 50vh var(--bg)',
+  }
+]
+
+let menuCloseBgKeyframes: Keyframe[] = [
+  menuOpenBgKeyframes[1],
+  menuOpenBgKeyframes[0]
+]
+
+function menuOpenBg() {
+  document.body.animate(menuOpenBgKeyframes, { duration: 600, easing: 'cubic-bezier(0.25, 0.25, 0, 1)' });
+  document.body.setAttribute('style', 'background-position: -32px 0px; background: box-shadow: inset 0px 0px 0px 50vh var(--bg)');
+}
+
+function menuCloseBg() {
+  document.body.animate(menuCloseBgKeyframes, { duration: 600, easing: 'cubic-bezier(0.25, 0.25, 0, 1)' });
+  document.body.setAttribute('style', '');
+}
+
 // TODO: it's heavely related to pages
-byId('rg')!.onmouseover = function () { byId('cnt')!.classList.add('navOpened', 'navTransformed'); }
-byId('rg')!.onmouseleave = function () { byId('cnt')!.classList.remove('navOpened'); }
+byId('rg')!.onmouseover = function () {
+  byId('cnt')!.classList.add('navOpened', 'navTransformed');
+  menuOpenBg();
+}
+byId('rg')!.onmouseleave = function () { byId('cnt')!.classList.remove('navOpened'); menuCloseBg() }
 
 let isNavHovered = false;
 
@@ -174,10 +203,12 @@ byId('nav')!.onclick = function () {
   if (!byId('cnt')!.classList.contains('navTapped')) {
     byId('cnt')!.classList.add('navTapped', 'navTransformed');
     tagById('nav', 'text')!.innerHTML = 'Close';
+    menuOpenBg()
   }
   else {
     byId('cnt')!.classList.remove('navTapped');
     tagById('nav', 'text')!.innerHTML = 'Navigation';
+    menuCloseBg();
   }
 }
 
@@ -188,14 +219,13 @@ document.body.onclick = function () {
   if (!isNavHovered) {
     byId('cnt')!.classList.remove('navTapped');
     tagById('nav', 'text')!.innerHTML = 'Navigation';
-
   }
 }
 
 var blurInvoked = false;
 
 let motionStart = function (event) {
-  if (!byId('cnt')?.classList.contains('navTapped')) return;
+  if (!byId('cnt')?.classList.contains('navTransformed')) return;
 
   if (!blurInvoked) {
     blurInvoked = true;

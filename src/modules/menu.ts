4dae45c0801d75cc1f24f654ca865e978c92a16f
Oleksii Besida <pixel.coder7@gmail.com>
2022-10-02 10:55:39 +0200

    improved motionblur handling

--- src/modules/menu.ts
+++ src/modules/menu.ts
@@ -8,100 +8,109 @@ import { byId, tagById } from "./shorthands";
 
 let menuOpenBgKeyframes: Keyframe[] = [
   {
-    backgroundPosition: '0px 0px',
-    boxShadow: 'inset 0px 0px 0px 50vh #FFF0',
+    backgroundPosition: "0px 0px",
+    boxShadow: "inset 0px 0px 0px 50vh #FFF0",
   },
   {
-    backgroundPosition: '-32px 0px',
-    boxShadow: 'inset 0px 0px 0px 50vh var(--bg)',
-  }
-]
+    backgroundPosition: "-32px 0px",
+    boxShadow: "inset 0px 0px 0px 50vh var(--bg)",
+  },
+];
 
 let menuCloseBgKeyframes: Keyframe[] = [
   menuOpenBgKeyframes[1],
-  menuOpenBgKeyframes[0]
-]
+  menuOpenBgKeyframes[0],
+];
 
 var i = 0;
-for(let child of byId('rg')!.children) {
+for (let child of byId("rg")!.children) {
   let curr = ++i;
-  child.addEventListener('mouseover', () => {
-    for(let subling of byId('rg')!.children)
-      subling.classList.remove('hover');
+  child.addEventListener("mouseover", () => {
+    for (let subling of byId("rg")!.children) subling.classList.remove("hover");
 
-    child.classList.add('hover')
+    child.classList.add("hover");
   });
 }
 
 function menuOpenBg(mouse: boolean) {
-  document.body.animate(menuOpenBgKeyframes, { duration: 600, easing: 'cubic-bezier(0.25, 0.25, 0, 1)' });
-  document.body.setAttribute('style', 'background-position: -32px 0px; background: box-shadow: inset 0px 0px 0px 50vh var(--bg)');
+  document.body.animate(menuOpenBgKeyframes, {
+    duration: 600,
+    easing: "cubic-bezier(0.25, 0.25, 0, 1)",
+  });
+  document.body.setAttribute(
+    "style",
+    "background-position: -32px 0px; background: box-shadow: inset 0px 0px 0px 50vh var(--bg)"
+  );
 }
 
 function menuCloseBg(mouse: boolean) {
-  document.body.animate(menuCloseBgKeyframes, { duration: 600, easing: 'cubic-bezier(0.25, 0.25, 0, 1)' });
-  document.body.setAttribute('style', '');
+  document.body.animate(menuCloseBgKeyframes, {
+    duration: 600,
+    easing: "cubic-bezier(0.25, 0.25, 0, 1)",
+  });
+  document.body.setAttribute("style", "");
 
-  for(let child of byId('rg')!.children)
-    child.classList.remove('hover');
+  for (let child of byId("rg")!.children) child.classList.remove("hover");
 }
 
-byId('rg')!.onmouseover = function (event) {
-  if (!byId('cnt')!.classList.contains('navTapped')) {
-    byId('cnt')!.classList.add('navOpened', 'navTransformed');
-    byId('rg')!.classList.add('hover');
+byId("rg")!.onmouseenter = function (event) {
+  if (!byId("cnt")!.classList.contains("navTapped")) {
+    byId("cnt")!.classList.add("navOpened", "navTransformed");
+    byId("rg")!.classList.add("hover");
     menuOpenBg(true);
 
-    let hovered = false;
-    for(let child of byId('rg')!.children) {
-      hovered = child.classList.contains('hover') || hovered;
-    
+    var hovered = false;
+    for (let child of byId("rg")!.children) {
+      hovered = child.classList.contains("hover") || hovered;
+    }
+
     if (!hovered) {
-      if (event.clientY < window.innerHeight/2) byId('rg')!.firstElementChild?.classList.add('hover')
-      else byId('rg')!.lastElementChild?.classList.add('hover')
+      if (event.clientY < window.innerHeight / 2)
+        byId("rg")!.firstElementChild?.classList.add("hover");
+      else byId("rg")!.lastElementChild?.classList.add("hover");
     }
   }
-}
-byId('rg')!.onmouseleave = function () { 
-  byId('cnt')!.classList.remove('navOpened'); 
-  byId('rg')!.classList.remove('hover');
+};
+byId("rg")!.onmouseleave = function () {
+  byId("cnt")!.classList.remove("navOpened");
+  byId("rg")!.classList.remove("hover");
   menuCloseBg(true);
-}
+};
 
 let isNavHovered = false;
 
-byId('nav')!.onclick = function (event) {
-  if (!byId('cnt')!.classList.contains('navTapped')) {
-    byId('cnt')!.classList.add('navTapped', 'navTransformed');
-    tagById('nav', 'text')!.innerHTML = 'Close';
-    menuOpenBg(false)
-  }
-  else {
-    byId('cnt')!.classList.remove('navTapped');
-    tagById('nav', 'text')!.innerHTML = 'Navigation';
+byId("nav")!.onclick = function (event) {
+  if (!byId("cnt")!.classList.contains("navTapped")) {
+    byId("cnt")!.classList.add("navTapped", "navTransformed");
+    tagById("nav", "text")!.innerHTML = "Close";
+    menuOpenBg(false);
+  } else {
+    byId("cnt")!.classList.remove("navTapped");
+    tagById("nav", "text")!.innerHTML = "Navigation";
     menuCloseBg(false);
   }
-}
+};
 
-byId('nav')!.onmouseover = () => isNavHovered = true;
-byId('nav')!.onmouseleave = () => isNavHovered = false;
+byId("nav")!.onmouseover = () => (isNavHovered = true);
+byId("nav")!.onmouseleave = () => (isNavHovered = false);
 
 document.body.onclick = function () {
   if (!isNavHovered) {
-    byId('cnt')!.classList.remove('navTapped');
-    tagById('nav', 'text')!.innerHTML = 'Navigation';
+    byId("cnt")!.classList.remove("navTapped");
+    tagById("nav", "text")!.innerHTML = "Navigation";
   }
-}
+};
 
-let navBlur = new MotionBlur({ blurID: 'cnt', watchID:'nav-work', mult: 1 })
+let navBlur = new MotionBlur({ blurID: "cnt", watchID: "nav-work", mult: 1 });
 
 let motionStart = function () {
-  if (!byId('cnt')?.classList.contains('navTransformed')) return;
+  if (!byId("cnt")?.classList.contains("navTransformed")) return;
+  navBlur.mult = window.innerWidth < 920 ? 1 : 0.5;
   navBlur.invoke();
-}
+};
 
-byId('rg')!.ontransitionrun = motionStart;
-byId('rg')!.onanimationstart = motionStart;
+byId("rg")!.ontransitionrun = motionStart;
+byId("rg")!.onanimationstart = motionStart;
 
-byId('rg')!.ontransitionend = navBlur.drop;
-byId('rg')!.onanimationend = navBlur.drop;
+byId("rg")!.ontransitionend = () => navBlur.drop();
+byId("rg")!.onanimationend = () => navBlur.drop();
--- src/modules/motionBlur.ts
+++ src/modules/motionBlur.ts
@@ -8,30 +8,35 @@ export default class MotionBlur {
 
   mult: number;
 
-  constructor(config: {blurID: string, watchID: string, mult: number}) {
-    this.blurID = config.blurID, this.watchID = config.watchID, this.mult = config.mult;
+  constructor(config: { blurID: string; watchID: string; mult: number }) {
+    this.blurID = config.blurID;
+    this.watchID = config.watchID;
+    this.mult = config.mult;
   }
 
   invoke() {
+    if (this.blurInvoked) return;
+
     this.blurInvoked = true;
     this.previous = byId(this.watchID)!.getBoundingClientRect();
-    requestAnimationFrame(this.motionBlur)
+    requestAnimationFrame(() => this.motionBlur());
   }
 
   drop() {
     this.blurInvoked = false;
-    byId(this.blurID)!.setAttribute('style', '');
+    byId(this.blurID)!.setAttribute("style", "");
   }
 
   motionBlur() {
     if (!this.blurInvoked) return;
 
     let current = byId(this.watchID)!.getBoundingClientRect();
-    let diff = Math.round(Math.abs(current.left - this.previous.left) * this.mult * 5) / 5;
+    let diff = Math.abs(current.left - this.previous.left);
+    let blur = Math.round(diff * this.mult * 5) / 5;
 
     this.previous = current;
 
-    byId(this.blurID)!.setAttribute('style', `filter: blur(${diff}px)`);
-    requestAnimationFrame(this.motionBlur);
+    byId(this.blurID)!.setAttribute("style", `filter: blur(${blur}px)`);
+    requestAnimationFrame(() => this.motionBlur());
   }
 }

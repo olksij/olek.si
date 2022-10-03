93a1ae76ff378945fd58a1c8e3aed4bac4856405
Oleksii Besida <pixel.coder7@gmail.com>
2022-10-03 22:57:11 +0200

    dynamic dom builder

--- src/modules/skeleton-loader.ts
+++ src/modules/skeleton-loader.ts
@@ -1,8 +1,8 @@
 window['worker'] = new Worker(new URL('../compute/compute.ts', import.meta.url), { type: 'module' });
 
-export var skeletonResolve, skeleton = new Promise((resolve) => skeletonResolve = resolve);
+var skeletonResolve, skeleton = new Promise((resolve) => skeletonResolve = resolve);
 
-var anim = [[{ backgroundPosition: '75vw 0, 0 0' }], { duration: 1500, iterations: Infinity }];
+var anim: [Keyframe[], Object] = [[{ backgroundPosition: '75vw 0, 0 0' }], { duration: 1500, iterations: Infinity }];
 
 document.addEventListener("DOMContentLoaded", async () => {
   // timeout counter
@@ -10,19 +10,19 @@ document.addEventListener("DOMContentLoaded", async () => {
   // iterate over skeleton loader elements
   ["pf", "tt", "d1", "d2", "ps", "nav", "rg", "ft"].forEach((id) => {
     // HTMLElement
-    var element = document.getElementById(id);
+    var element = document.getElementById(id)!;
     // make it iterable ..
-    element = (["ps", "rg", "ft"].includes(id) ? element.children : [element]);
+    var elements = (["ps", "rg", "ft"].includes(id) ? element.children : [element]);
     // .. & iterate
-    for (var item of element) {
+    for (var item of elements) {
       // to render mark
       item.classList.add("tr");
       // replace "tr" with "psl" - prepeared skeleton loader
       setTimeout((item) => item.classList.replace("tr", "sl"), cnt, item);
       // we can try to animate it already
-      item.animate(...anim);
+      item.animate(anim[0], anim[1]);
       // increase wait counter
-      cnt += 100 / element.length;
+      cnt += 100 / elements.length;
     }
   });
   setTimeout(skeletonResolve, cnt);
@@ -35,7 +35,7 @@ let fonts = {
 };
 
 // and resolved ones
-let fontResult = {};
+let fontResult: Record<string, ArrayBuffer> = {};
 
 for (let font in fonts) {
   // create a request
@@ -49,7 +49,20 @@ for (let font in fonts) {
     fontResult[font] = res;
     // if all fonts are there, postMessage
     if (fontResult.display && fontResult.text)
-      worker.postMessage({ deliver: 'fonts', data: fontResult }, fontResult);
+    window['worker'].postMessage({ deliver: 'fonts', data: fontResult }, fontResult);
   }
   request.send();
+}
+
+const rg = { home: {}, about: {}, projects: {}, work: {} }
+
+export function buildTree(dom: any, parent: HTMLElement = document.body) {
+  console.log(dom, parent)
+  for (let elementID of Object.keys(dom)) {
+    var child = document.createElement('div');
+    child.id = elementID, parent.append(child);
+    
+    if (elementID == 'rg') return buildTree(rg, parent);
+    buildTree(dom[elementID], child);
+  }
 }

8ee6b2be72ba902d316e0875914b98d8802c7b28
Oleksii Besida <pixel.coder7@gmail.com>
2022-11-01 12:14:07 +0100

    placeholder generation with border radius

--- src/compute/metrics.ts
+++ src/compute/metrics.ts
@@ -17,11 +17,26 @@ export default function matrics(fonts: FontsRecord<'computed'>, data: InputMorph
   else if (from.size) {
     // letter width for placeholder
     let lw = Math.round(from.size[0] / path.length * 100) / 100;
+
+    let br = 4;
+
     // split placeholder rectangle for each letter
-    for (var ww = 0, i = 0; i < path.length; ww += lw, i++) {
+    fromPath.push(`M0 ${br} A${br} ${br} 0 0 1 ${br} 0 H${lw} V${from.size[1]} H${br} A${br} ${br} 0 0 1 0 ${from.size[1]-br} V${br} Z `);
+
+    for (var ww = lw, i = 1; i < path.length-1; ww += lw, i++) {
       let currPath = `M ${ww} 0 V${from.size[1]} H${ww + lw} V0 H${ww} Z`;
       fromPath.push(currPath);
     }
+
+    fromPath.push(`
+      M${lw*(path.length-1)} 0 
+      H${lw*path.length-br} 
+      A${br} ${br} 0 0 1 ${lw*path.length} ${br}
+      V${from.size[1]-br} 
+      A${br} ${br} 0 0 1 ${lw*path.length-br} ${from.size[1]} 
+      H${lw*(path.length-1)} 
+      V0 
+      Z `);
   }
 
   while (fromPath.length < path.length)

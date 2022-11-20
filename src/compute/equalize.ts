3231e29dec2b309c41a4328fb64ac8ac6c8e7686
Oleksii Besida <pixel.coder7@gmail.com>
2022-11-20 14:40:09 +0100

    fixed rotations

--- src/compute/equalize.ts
+++ src/compute/equalize.ts
@@ -1,9 +1,11 @@
 import { svgPathProperties } from "svg-path-properties";
+import { polygonArea } from "d3-polygon";
 
 export default function(fromPath: string[], toPath: string[]) {
   let from = "", to = "";
 
   let startTime = performance.now()
+  
 
   // since [fromPath.length] and [toPath.length] are equal
   for (let i = 0; i < fromPath.length; i++) {
@@ -11,17 +13,24 @@ export default function(fromPath: string[], toPath: string[]) {
     let toProps = new svgPathProperties(toPath[i]);
     let startLength = 0, startDistance = Infinity;
 
-    for (let length = 0; length <= toProps.getTotalLength(); length += 1) {
-      let command = length == 0 ? "M" : "L";
-      let coordinates = toProps.getPointAtLength(toProps.getTotalLength() - length);
-      to += `${command}${Math.round(coordinates.x*100)/100} ${Math.round(coordinates.y*100)/100}`;
+    let points: [number, number][] = [];
 
-      let coordinatesF = fromProps.getPointAtLength(0);
-      let distance = Math.sqrt(Math.pow(coordinates.x-coordinatesF.x,2)+Math.pow(coordinates.y-coordinatesF.y,2));
-      //if (distance < startDistance)
-      //  startLength = length, startDistance = distance;
-      startLength = toProps.getTotalLength()/2;
+    for (let length = 0; length <= toProps.getTotalLength(); length += 1) {
+      let position = toProps.getPointAtLength(toProps.getTotalLength() - length);
+      points.push([Math.round(position.x*100)/100, Math.round(position.y*100)/100])
+
+      let pos2 = fromProps.getPointAtLength(0);
+      let distance = Math.sqrt(Math.pow(position.x-pos2.x,2)+Math.pow(position.y-pos2.y,2));
+      if (distance < startDistance)
+        startLength = length, startDistance = distance;
     }
+
+    console.log(polygonArea(points))
+    if (polygonArea(points) < 0) points.reverse();
+    else startLength = toProps.getTotalLength()/2;
+
+    to += `M${points[0][0]} ${points[0][1]}`, points.splice(0, 1);
+    points.forEach(p => to += `L${p[0]} ${p[1]}`);
 
     for (let point = 0; point <= toProps.getTotalLength(); point++) {
       let length = ((startLength + point) % toProps.getTotalLength())

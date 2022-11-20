7aa477883be4dd23d59b3ee2ff2cc583cddb0e4d
Oleksii Besida <pixel.coder7@gmail.com>
2022-11-20 21:57:25 +0100

    rotating in the right way

--- src/compute/normalize.ts
+++ src/compute/normalize.ts
@@ -21,14 +21,38 @@ export default function(fromPath: string[], toPath: string[]) {
     let toRing: PathRing = [], 
         fromRing: PathRing = [];
     
+    let total = toProps.getTotalLength();
+    let points = Math.floor(total / precision);
+  
     // construct rings
-    for (let length = 0; length <= toProps.getTotalLength(); length += precision) {
+    for (let length = 0; length <= total; length += precision) {
       toRing.push(getPoint(toProps.inst, length));
-      fromRing.push(getPoint(fromProps.inst, length));
+      fromRing.push(getPoint(fromProps.inst, length / total * fromProps.getTotalLength()));
     }
 
     // rotate rings so they are clockwise
     if (polygonArea(toRing) < 0) toRing.reverse();
+    if (polygonArea(fromRing) < 0) fromRing.reverse();
+
+    if (total) {
+      let minSum = Infinity, bestOffset;
+
+      for (var offset = 0; offset <= points; offset++) {
+        let sum = 0;
+
+        fromRing.forEach((point, i) => {
+          var d = distance(toRing[(i + offset) % points], point);
+          sum += d * d;
+        });
+    
+        if (sum < minSum) minSum = sum, bestOffset = offset;
+      }
+      
+      if (bestOffset) {
+        let spliced = toRing.splice(0, bestOffset);
+        toRing.splice(toRing.length, 0, ...spliced);
+      };
+    }
     
     // add stringified path to result
     to += ringToString(toRing);
@@ -50,6 +74,10 @@ function ringToString(ring: [number, number][]) {
   let string = `M${ring[0][0]} ${ring[0][1]}`;
   ring.splice(0, 1), ring.forEach(p => string += `L${p[0]} ${p[1]}`);
   return string + 'Z';
+}
+
+export function distance(a: PathPoint, b: PathPoint) {
+  return Math.sqrt((a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1]));
 }
 
 const decimals = Math.pow(10, 2);

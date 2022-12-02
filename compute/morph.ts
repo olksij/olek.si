6954da179f8d9ccced7e13477589b0b7d8f59742
Oleksii Besida <pixel.coder7@gmail.com>
2022-12-02 21:41:55 +0100

    computed text wrap dust

--- compute/morph.ts
+++ compute/morph.ts
@@ -13,14 +13,14 @@ export default async function interpolate(request: string, data: ComputeRequest,
   let { from, to } = data;
 
   // vectorize the [to] element
-  let { path, baseline, width } = vectorize(to, fonts)!;
+  let { path, baseline, width } = vectorize(to, from.skeleton, fonts)!;
 
   // get [PathRings[]] out of PathData string
   let toRings = convert(path);
 
   // if there is a predefined from element, animate from it
   let fromRings = from.element || from.path
-    ? convert((vectorize(from.element, fonts) ?? from).path!, toRings) 
+    ? convert((vectorize(from.element, from.skeleton, fonts) ?? from).path!, toRings) 
     : skeleton(data, toRings); // else, build a skeleton
 
   //           rearrage points in rings for smooth animation
--- compute/vectorize.ts
+++ compute/vectorize.ts
@@ -1,7 +1,7 @@
 import { Font } from "opentype.js";
-import { FontsRecord, FontStyle, MorphElement } from "/interfaces";
+import { FontsRecord, FontStyle, MorphElement, SkeletonBaseConfig } from "/interfaces";
 
-export default function (element: MorphElement | undefined, fonts: FontsRecord) {
+export default function (element: MorphElement | undefined, skeleton: SkeletonBaseConfig, fonts: FontsRecord) {
   if (!element) return undefined;
 
   let icon = element.icon, text = element.text;
@@ -20,11 +20,25 @@ export default function (element: MorphElement | undefined, fonts: FontsRecord) 
 
     let textLeft = icon ? icon.gap + (icon.height ?? style.height) : 0;
 
-    baseline = calculateBaseline(font, style);
-    let pathData = font.getPath(text?.text, textLeft, baseline, style.fontSize, { letterSpacing: style.letterSpacing });
-    width = pathData.getBoundingBox().x2;
+    let words = text.text.split(' '), lines: {text: string[], width: number}[] = [{text: [], width: 0}];
+    let whitespace = font.getAdvanceWidth(' ', style.fontSize);
 
-    pathString += pathData.toPathData(2);
+    words.forEach(word => {
+      let width = font.getAdvanceWidth(word, style.fontSize),
+          line = lines.length - 1, spaced = whitespace + width;
+      
+      if (lines[line].width + spaced <= skeleton[0]! || !style.wrap)
+        lines[line].text.push(word), lines[line].width += spaced;
+
+      else lines.push({ text: [word], width })
+    });
+
+    // unless multiline text is enabled, propose a new width for computed text
+    width = style.wrap ? skeleton[0]! : lines[0].width;
+    baseline = calculateBaseline(font, style);    
+
+    lines.forEach((line, i) => pathString += font.getPath(line.text.join(' '), 
+      textLeft, baseline + style.height * i, style.fontSize, { letterSpacing: style.letterSpacing }).toPathData(2));
   }
 
   return { path: pathString, baseline, width, points };

20ca38ce10abd1ade6488631900e27ae4b0d96c2
Oleksii Besida <pixel.coder7@gmail.com>
2022-12-04 16:00:01 +0100

    some styles improvements

--- about/dom.ts
+++ about/dom.ts
@@ -2,13 +2,13 @@ import { SkeletonTree } from "interfaces";
 
 const aboutDom: SkeletonTree = {
   cnt: {
-    title: { tt: [[220, 64, 18], [384, 96, 24]] },
+    tt: [[220, 64, 18], [384, 96, 24]],
     dd: { d1: [[288, 24, 8], [320, 28, 8]], d2: [[128, 24, 8], [256, 28, 8]] },
   },
   nav: [[112, 24, 8]],
   page: {
     bio: [[null, 200, 8]],
-    image: [[240, 240, 8]],
+    image: [[240, 240, 8], [null, null, 8]],
   },
   ft: { config: [[96, 16, 8], [128, 16, 8]], cr: {}, lg: {} }
 };

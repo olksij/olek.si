ba784c42355d128b2dfa4869328ad1305a21a76e
Oleksii Besida <besida@oleksii.xyz>
2023-09-26 16:27:00 +0000

    improved animation easings

--- src/signature.js
+++ src/signature.js
@@ -3,11 +3,24 @@ import animationJson from 'bundle-text:../assets/signature.txt';
 
 export function fireAnimation() {
   const player = document.querySelector("lottie-player");
+
   player.load(animationJson);
+  player.animate(keyframes, timing);
 
   setTimeout(() => {
     player.classList.add('rendered');
     document.getElementById('footer').classList.remove('torender');
     document.getElementById('footer').classList.add('rendered');
-  }, 3800);
+  }, 1800);
 }
+
+const keyframes = [
+  { opacity: "0" },
+  { opacity: "1" },
+];
+
+const timing = {
+  duration: 1000,
+  iterations: 1,
+  easing: 'cubic-bezier(0.5, 0, 0, 1)',
+};
